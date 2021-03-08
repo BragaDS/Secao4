ee4b345095b2078fb28dda852d27587a4b18aeb7
WoKRamon <79801558+WoKRamon@users.noreply.github.com>
2021-03-08 03:08:38 -0300

    Exercicios

--- exercicio47.py
+++ exercicio47.py
@@ -1,11 +1,9 @@
 print("Digite um numero de 1000 a 9999")
 num = int(input())
 
-
 while num < 1000 or num > 9999:
     print("Digite um numero de 1000 a 9999")
     num = int(input())
-
 
 print(f"primeiro digito é {str(num)[0]}")
 print(f"segundo digito é {str(num)[1]}")
